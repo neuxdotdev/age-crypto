7eee9433709f0ca9989911bd4435bc984b21d8fd
neuxdotdev <neuxdev1@gmail.com>
2026-05-12 19:19:56 +0700

    refactor(docs): remove verbose doc comments from passphrase decrypt
    Strip detailed doc comments from decrypt_with_passphrase.rs
    and decrypt_with_passphrase_armor.rs.
    
    Simplify files; documentation centralized in lib.rs.
    
    No functional change.
    
    Refs: none
    
    Test: cargo build
    
    BREAKING CHANGE: None

--- src/apis/decrypt_with_passphrase_armor.rs
+++ src/apis/decrypt_with_passphrase_armor.rs
@@ -1,23 +1,13 @@
-//! Passphrase‑based decryption of armored age ciphertexts.
-
 use crate::apis::decrypt_with_passphrase;
 use crate::errors::Result;
 use crate::errors::decrypt::DecryptError;
 use age::armor::ArmoredReader;
 use std::io::Read;
-
-/// Decrypts an armor‑encoded age ciphertext that was encrypted with a passphrase.
-///
-/// This function removes the armor using [`ArmoredReader`] and then decrypts
-/// the resulting binary ciphertext with [`decrypt_with_passphrase`].
 pub fn decrypt_with_passphrase_armor(armored: &str, passphrase: &str) -> Result<Vec<u8>> {
-    // Step 1: remove armor to get binary ciphertext
     let mut reader = ArmoredReader::new(armored.as_bytes());
     let mut ciphertext = Vec::new();
     reader
         .read_to_end(&mut ciphertext)
         .map_err(|e| DecryptError::InvalidCiphertext(e.to_string()))?;
-
-    // Step 2: binary decryption
     decrypt_with_passphrase(&ciphertext, passphrase)
 }
